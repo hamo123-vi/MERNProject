81c2d7654a22271da98fa4e0a4579f81c8b86bd6
Amil Valjevac <amil.valjevac@cloudit.local>
2022-02-04 08:58:05 +0100

    Config file set

--- app.js
+++ app.js
@@ -1,13 +1,22 @@
 // app.js
 
 const express = require('express');
+const dotenv = require('dotenv');
+var cors = require('cors');
 const connectDB = require('./config/db');
-const books = require('./routes/api/books')
+const books = require('./routes/api/books');
+
+dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+
+
 // Connect Database
 connectDB();
+
+// Use CORS
+app.use(cors({credentials: true, origin: true}))
 
 // Body parser
 app.use(express.json())
--- config/db.js
+++ config/db.js
@@ -1,20 +1,14 @@
 const mongoose = require('mongoose');
-const config = require('config');
-const db = config.get("MONGO_URI");
 
 const connectDb = async () => {
-    try {
-        await mongoose.connect(
-            db,
+        const conn = await mongoose.connect(
+            process.env.MONGO_URI,
             {
                 useNewUrlParser: true
             }
         );
         console.log("Connected to database!");
-    } catch(err) {
-        console.log(err.message);
-        process.exit(1);
-    }
+    
 }
 
 module.exports = connectDb;
